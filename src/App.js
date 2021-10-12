2b11aa53b61e6add83bc811a76c9fd9350d8764a
CTW100 <85342844+CTW100@users.noreply.github.com>
2021-10-13 02:22:55 +0900

    64
    Using Stateful Lists

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
+import { useState } from 'react';
+
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Hannam The Hill',
+    amount: 9912.34,
+    date: new Date(2025, 1, 18),
+  },
+  {
+    id: 'e2',
+    title: 'Rolex',
+    amount: 3212.34,
+    date: new Date(2023, 9, 28),
+  },
+  {
+    id: 'e3',
+    title: 'Rolls Roys',
+    amount: 6743.34,
+    date: new Date(2024, 5, 18),
+  },
+  {
+    id: 'e4',
+    title: 'Happiness',
+    amount: 0.01,
+    date: new Date(2000, 1, 18),
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Hannam The Hill',
-      amount: 9912.34,
-      date: new Date(2025, 1, 18),
-    },
-    {
-      id: 'e2',
-      title: 'Rolex',
-      amount: 3212.34,
-      date: new Date(2023, 9, 28),
-    },
-    {
-      id: 'e3',
-      title: 'Rolls Roys',
-      amount: 6743.34,
-      date: new Date(2024, 5, 18),
-    },
-    {
-      id: 'e4',
-      title: 'Happiness',
-      amount: 0.01,
-      date: new Date(2000, 1, 18),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
