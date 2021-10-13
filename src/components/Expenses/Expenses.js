908c887332b21eb7ac7718f1d88d5ad25e8a71b2
CTW100 <85342844+CTW100@users.noreply.github.com>
2021-10-13 14:03:18 +0900

    Assignment4

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,14 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
 import './Expenses.css';
 
-function Expenses(props) {
+const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = (selectedYear) => {
-    console.log('Expenses.js');
     setFilteredYear(selectedYear);
   };
 
@@ -29,6 +27,6 @@ function Expenses(props) {
       </Card>
     </div>
   );
-}
+};
 
 export default Expenses;
